f468b0bc0e74c3548272f28ab7d17d99e60f7cfd
Prabha Pothen <prabha.pothen@gmail.com>
2021-05-19 15:34:32 +0200

    deleted review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_19_124046) do
+ActiveRecord::Schema.define(version: 2021_05_19_124720) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_05_19_124046) do
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "phone"
   end
 
   create_table "reviews", force: :cascade do |t|
