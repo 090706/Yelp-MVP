90913e2c3b7df2cfadd1dffde3d3071d6ad62617
Prabha Pothen <prabha.pothen@gmail.com>
2021-05-19 18:53:25 +0200

    saving progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "restaurants", to: "restaurants#index"
-  # resources :restaurants do
-  #   collection do
-  #     get :top
-  #   # end
-  # end
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new'
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:create]
+  end
 end
